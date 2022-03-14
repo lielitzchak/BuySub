eef641f4711ee4ad30e081123f81fa160491eb36
liel itzchak <liel.izcha321@gmail.com>
2022-03-14 12:19:05 +0200

    add components and clean app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import RestApiBarcode from "./Components/lielTest";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <RestApiBarcode />
       </header>
     </div>
   );
