b4a3ca091d98a8c451913b0fd4c1d61ab1c79874
ascha10 <ascha9957@gmail.com>
2022-03-20 19:12:03 +0200

    setting group added

--- client/src/Components/Pages/Setting/Setting.component.jsx
+++ client/src/Components/Pages/Setting/Setting.component.jsx
@@ -1,15 +1,42 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { authContext } from "../../../Context/AuthProvider.component";
+import { getGroupInfo } from "../../../Services/GroupsService.service";
 
 
 export default function Setting() {
   const { auth } = useContext(authContext);
+  const [groupInfo,setGroupInfo] = useState({});
+
+  useEffect(() => {
+
+    getGroupInfo(auth.groupName).then((data) => {
+         setGroupInfo(data)
+         console.log(data);
+    })
+  },[])
+
   return (
     <section>
       <h1>Setting</h1>
-      <h1>Group Name : {auth.groupName}</h1>
-      {/* <h1>Members :{auth.members && auth.members.length ? auth.members.length : 0}</h1>
-      <h1>Numbers Of Products : {auth.products && auth.products.length ? auth.products.length : 0}</h1> */} 
+      <img src={groupInfo.imageGroup} alt="group" />
+      
+      <h1>Number Of Products : {groupInfo.products ? groupInfo.products.length : 0}</h1>
+      <h1>Group Name : {groupInfo.groupName}</h1>
+      <h1>CreatedAt : {groupInfo.createdAt}</h1>
+      <section>
+        <h1>Group Members </h1> 
+         {groupInfo.members ? groupInfo.members.map((userMember,index) => {
+           return (
+              <section key={index}>
+                    <img src={userMember.image} alt="user" />
+                    <h1>firstName : {userMember.firstName}</h1>
+                    <h1>lastName : {userMember.lastName}</h1>
+                    <h1>isLogin : {userMember.isLogin ? '✔' : 'false' }</h1>
+              </section>
+           )
+         }) : <h1>None</h1>}
+      </section>
+
       
       </section>
   )
--- server/Routes/groups.js
+++ server/Routes/groups.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {getGroups,getGroupById,getGroupProducts,addGroup,joinGroup,updateGroup,deleteGroup} = require('../Controllers/groupsController');
+const {getGroups,getGroupById,getGroupProducts,getGroupInfo,addGroup,joinGroup,updateGroup,deleteGroup} = require('../Controllers/groupsController');
 
 
 router.get('/groups',getGroups)
@@ -8,6 +8,8 @@ router.get('/groups',getGroups)
 router.get('/groups/:id',getGroupById)
 
 router.get('/groups/products/:groupName',getGroupProducts)
+
+router.get('/groups/groupInfo/:groupName',getGroupInfo)
 
 router.post('/groups/:id',addGroup)
 
