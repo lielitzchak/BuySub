a745ee2fa00d9a99e6a0fed6f5b3a86db7ecbf82
ascha10 <ascha9957@gmail.com>
2022-03-22 12:48:56 +0200

    redirect fixed

--- client/src/Components/Pages/Permission/Permission.jsx
+++ client/src/Components/Pages/Permission/Permission.jsx
@@ -6,7 +6,7 @@ export default function Permission({allowedRole}) {
     const {auth} = useContext(authContext)
 
   return (
-    auth.role && auth.role.length >= 1 && auth.role.includes(allowedRole) ? <Outlet/> :  
+    auth.role && auth.role.length >= 1 && auth.role.includes(allowedRole) ? <Outlet/> : 
     auth.email ? <Navigate to="Unauthorized" /> : <Navigate to="/Login"/>
   )
 }
