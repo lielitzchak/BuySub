f7364066fc7d0dbf78084ab7476d4eac8e911580
ascha10 <ascha9957@gmail.com>
2022-03-23 22:34:26 +0200

    remove admin in admin comp

--- client/src/Components/Pages/Admin/Admin.component.tsx
+++ client/src/Components/Pages/Admin/Admin.component.tsx
@@ -1,9 +1,66 @@
-import { Link, Outlet } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import { authContext } from "../../../Context/AuthProvider.component";
+import { getGroupInfo } from "../../../Services/GroupsService.service";
+import { adminRemoveMember } from "../../../Services/GroupsService.service";
+
 export const Admin = (): JSX.Element => {
+
+  const { auth } = useContext(authContext);
+  const [groupInfo,setGroupInfo] :any = useState({});
+
+  useEffect(() => {
+
+    getGroupInfo(auth.groupName).then((data) => {
+         setGroupInfo(data)
+         console.log(data);
+    })
+
+  },[])
+
+  let addMembers = () => {
+    console.log('addMembers');
+    
+  }
+
+  let removeUser = async(id:any) => {
+    adminRemoveMember(id,auth.groupName).then((data) => {
+      console.log(data);
+      
+    }).catch((err) => {
+      console.log(err);
+        
+    })
+    
+  }
+
+  let addAsAdmin = () => {
+    console.log('addAsAdmin');
+    
+  }
+
   return (
     <>
-      <Link to="AddProduct">add product</Link>
-      <Link to="AddMembers">add members</Link>
+      <button onClick={addMembers}>Add Members +</button>
+
+      <section>
+        <h1>Group Members </h1> 
+         {groupInfo.members ? groupInfo.members.map((userMember: any,index :any) => {
+           return (
+              <section key={index}>
+                    <img src={userMember.image} alt="user" />
+                    <h1>FirstName : {userMember.firstName}</h1>
+                    <h1>LastName : {userMember.lastName}</h1>
+                    <h1>IsLogin : {userMember.isLogin ? '✔' : 'false' }</h1>
+                    <h1>id : {userMember._id}</h1>
+                    <h1>Role : {userMember.role[1]}</h1>
+                    <button onClick={() => removeUser(userMember._id)}>Remove User </button><br />
+                    <button onClick={addAsAdmin}>Add As Admin</button>
+
+              </section>
+           )
+         }) : <h1>None</h1>}
+      </section>
       <section>
         <Outlet />
       </section>
--- client/src/Services/GroupsService.service.ts
+++ client/src/Services/GroupsService.service.ts
@@ -69,3 +69,75 @@ export const updateGroup = async (id:any , groupToEdit:any): Promise<any> => {
     .catch((err) => console.log(err));
 };
 ////////////////////////////////////////////////////////////////////////
+
+
+export const adminRemoveMember = async (id:any,groupName: any): Promise<any> => {
+  let options = {
+    method: "PUT",
+    headers: { 
+      "content-type": "application/json",
+      "Authorization": `Bearer ${localStorage.getItem('jwtToken')}`
+     },
+    // body: JSON.stringify(groupNameToEdit),
+  };
+  return await fetch(`${basic_url}/groups/admin/removeMember/${id}/${groupName}`, options)
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
+};
+
+export const adminAddMember = async (groupName: any,userEmail: any): Promise<any> => {
+  let options = {
+    method: "POST",
+    headers: { 
+      "content-type": "application/json",
+      "Authorization": `Bearer ${localStorage.getItem('jwtToken')}`
+     },
+    body: JSON.stringify(userEmail),
+  };
+  return await fetch(`${basic_url}/groups/admin/adminAddMember/${groupName}`, options)
+    .then((res) => res.json())
+    .catch((er) => console.log(er));
+};
+
+export const adminAddAdmin = async (id: any): Promise<any> => {
+  let options = {
+    method: "POST",
+    headers: { 
+      "content-type": "application/json",
+      "Authorization": `Bearer ${localStorage.getItem('jwtToken')}`
+     },
+    // body: JSON.stringify(groupName),
+  };
+  return await fetch(`${basic_url}/groups/admin/adminAddAdmin/${id}`, options)
+    .then((res) => res.json())
+    .catch((er) => console.log(er));
+};
+
+
+export const adminRemoveAdmin = async (id:any): Promise<any> => {
+  let options = {
+    method: "PUT",
+    headers: { 
+      "content-type": "application/json",
+      "Authorization": `Bearer ${localStorage.getItem('jwtToken')}`
+     },
+    // body: JSON.stringify(groupNameToEdit),
+  };
+  return await fetch(`${basic_url}/groups/admin/adminRemoveAdmin/${id}`, options)
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
+};
+
+export const exitGroup = async (id:any, groupName:any): Promise<any> => {
+  let options = {
+    method: "PUT",
+    headers: { 
+      "content-type": "application/json",
+      "Authorization": `Bearer ${localStorage.getItem('jwtToken')}`
+     },
+    // body: JSON.stringify(groupNameToEdit),
+  };
+  return await fetch(`${basic_url}/groups/admin/exitGroup/${id}/${groupName}`, options)
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
+};
