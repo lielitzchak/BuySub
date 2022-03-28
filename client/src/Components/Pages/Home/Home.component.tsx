2a34ff2a0475550a0d66203d6f3557e4bd2ab842
liel itzchak <liel.izcha321@gmail.com>
2022-03-28 15:34:26 +0300

    footer

--- client/src/Components/Pages/Home/Home.component.tsx
+++ client/src/Components/Pages/Home/Home.component.tsx
@@ -1,6 +1,28 @@
-
 export default function Home() {
   return (
-    <div>Home Page</div>
-  )
+    <div>
+      <section className="aboutApp"></section>
+      <section className="programmers">
+        <img
+          src="https://media-exp1.licdn.com/dms/image/D4E35AQE_zb_J8vi1Uw/profile-framedphoto-shrink_400_400/0/1642446411789?e=1648548000&v=beta&t=_L_CBYPFnmBFtRsTtxuuw9r0WnmrLBNGjZAVr5k7S2k"
+          alt="Aschalew Azage"
+        />
+        <p>Aschalew Azage</p>
+        <img src="" alt="linkedin" />
+        <img src="" alt="github" />
+      </section>
+      <section className="programmers">
+        <img src="" alt="Eliyahu Belay" />
+        <p>Eliyahu Belay</p>
+        <img src="" alt="linkedin" />
+        <img src="" alt="github" />
+      </section>
+      <section className="programmers">
+        <img src="" alt="Liel Itzchak" />
+        <p>Liel Itzchak</p>
+        <img src="" alt="linkedin" />
+        <img src="" alt="github" />
+      </section>
+    </div>
+  );
 }
