6de3aa78565082fedaf627b59cc39cb507e806ca
eliyahubelay0@gmail.com <eliyahubelay0@gmail.com>
2022-03-29 19:15:43 +0300

    pop up edit group component

--- client/src/Components/Features/UpdateGroup/UpdateGroup.component.tsx
+++ client/src/Components/Features/UpdateGroup/UpdateGroup.component.tsx
@@ -1,9 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { updateGroup } from "../../../Services/GroupsService.service";
+// import Button from '@mui/material/Button';
+// import TextField from '@mui/material/TextField';
+// import Dialog from '@mui/material/Dialog';
+// import DialogActions from '@mui/material/DialogActions';
+// import DialogContent from '@mui/material/DialogContent';
+// import DialogContentText from '@mui/material/DialogContentText';
+// import DialogTitle from '@mui/material/DialogTitle';
+/////////////////////////////////////////////////////
+// import Button from '@mui/material/Button';
+// import Dialog, { DialogProps } from '@mui/material/Dialog';
+// import DialogActions from '@mui/material/DialogActions';
+// import DialogContent from '@mui/material/DialogContent';
+// import DialogContentText from '@mui/material/DialogContentText';
+// import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+
 
 export default function UpdateGroup(props: any) {
     const [groupDetail, setgroupDetail]: any = useState({});
     const [prevgrouptDetail, setPrevgrouptDetail]: any = useState(props.item);
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+    // const [open, setOpen] = useState(false);
+
+
 
 
     let updateGroupInfo = (event: any): void => {
@@ -22,25 +50,62 @@ export default function UpdateGroup(props: any) {
         updateGroup(props.item._id, groupDetail)
             .then((data) => {
                 console.log(data)
-                props.setshowUpdate(!props.showUpdate)
+                props.setOpen(!props.open)
             })
             .catch((err: any) => {
                 console.log(err);
             });
-
-
     }
 
-    let cancel = () => {
-        props.setshowUpdate(!props.showUpdate)
-    }
+    // const handleClose = () => {
+    //     props.setshowUpdate(!props.showUpdate);
+    //   };
+
+    // const handleClickOpen = () => {
+    //     setOpen(true);
+    //   };
+
+    const handleClose = () => {
+        props.setOpen(false);
+
+        // props.setShowUpdate(!props.showUpdate);
+    };
+
 
     return (
         <>
-            {props.showUpdate ?
-                <section>
-                    <h1>Update Group</h1>
-                    <form action="" autoComplete="on" onSubmit={editGroupInfo}>
+            {/* {props.showUpdate ? */}
+            <section style={{ backgroundColor: "red" }}>
+               
+                <form>
+                    <Dialog
+                        fullScreen={fullScreen}
+                        open={props.open}
+                        onClose={handleClose}
+                        aria-labelledby="responsive-dialog-title"
+                    >
+                        <DialogTitle id="responsive-dialog-title">
+                            {"Use Google's location service?"}
+                        </DialogTitle>
+                        <DialogContent>
+                            <DialogContentText>
+                                Let Google help apps determine location. This means sending anonymous
+                                location data to Google, even when no apps are running.
+                            </DialogContentText>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button autoFocus onClick={handleClose}>
+                                Disagree
+                            </Button>
+                            <Button onSubmit={editGroupInfo} autoFocus>
+                                Agree
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
+                </form>
+
+
+                {/* <form action="" autoComplete="on" onSubmit={editGroupInfo}>
 
                         <label>group Name</label>
                         <input type="text" name="groupName" placeholder={prevgrouptDetail.groupName} onChange={updateGroupInfo} required />
@@ -53,10 +118,12 @@ export default function UpdateGroup(props: any) {
 
                         <button>Update Group</button>
                     </form>
-                    <button onClick={cancel}>Cancel</button>
+                    <button >Cancel</button> */}
 
-                </section> :
-                <button onClick={() => props.setshowUpdate(!props.showUpdate)}>Edit Group Info</button>}
+            </section >
+            {/* <Button onClick={handleClose}>Edit Group Info</Button> */}
+            {/* } */}
+            {/* // <button onClick={() => props.setshowUpdate(!props.showUpdate)}>Edit Group Info</button> */}
         </>
 
     )
--- client/src/Components/Pages/Setting/Setting.component.jsx
+++ client/src/Components/Pages/Setting/Setting.component.jsx
@@ -1,66 +1,77 @@
 import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { authContext } from "../../../Context/AuthProvider.component";
-import { exitGroup,getGroupInfo } from "../../../Services/GroupsService.service";
+import { exitGroup, getGroupInfo } from "../../../Services/GroupsService.service";
 import UpdateGroup from "../../Features/UpdateGroup/UpdateGroup.component";
+import Button from '@mui/material/Button';
+
 
 
 export default function Setting() {
   const { auth } = useContext(authContext);
-  const [groupInfo,setGroupInfo] = useState({});
-  const [showUpdate, setshowUpdate] = useState(false)
+  const [groupInfo, setGroupInfo] = useState({});
+  const [open, setOpen] = useState(false)
+
+
+  // const [open, setOpen] = useState(false);
 
   const navigate = useNavigate()
 
   useEffect(() => {
 
     getGroupInfo(auth.groupName).then((data) => {
-         setGroupInfo(data)
-         console.log(data);
+      setGroupInfo(data)
+      console.log(data);
     })
-  },[])
+  }, [])
 
 
-  let exitTheGroup = ()=>{
-    exitGroup(auth.id,auth.groupName)
-    .then((data)=> console.log(data))
-    .then((err)=> console.log(err))
+  let exitTheGroup = () => {
+    exitGroup(auth.id, auth.groupName)
+      .then((data) => console.log(data))
+      .then((err) => console.log(err))
     navigate("/");
 
   }
- 
+
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
 
   return (
     <section>
       <h1>Setting</h1>
       <button onClick={exitTheGroup}>Exit The Group</button>
-      
 
-      {showUpdate ?<UpdateGroup item={groupInfo} showUpdate={showUpdate} setshowUpdate={setshowUpdate} /> : <button onClick={() => setshowUpdate(!showUpdate)}>Edit Group Info</button>}
+
+      {open ? <UpdateGroup item={groupInfo} open={open} setOpen={setOpen} /> : <Button variant="outlined" onClick={handleClickOpen}>Edit Group Info</Button>}
+
+      {/* : <button onClick={() => setshowUpdate(!showUpdate)}>Edit Group Info</button>
       
-      {/* <button onClick={showFormToEditGroup}>Edit Group Info</button> */}
+       <button onClick={showFormToEditGroup}>Edit Group Info</button> */ }
       <img src={groupInfo.imageGroup} alt="group" />
-      
+
       <h1>Number Of Products : {groupInfo.products ? groupInfo.products.length : 0}</h1>
       <h1>ListToBuy : {groupInfo.listToBuy ? groupInfo.listToBuy.length : 0}</h1>
       <h1>Group Name : {groupInfo.groupName}</h1>
       <h1>CreatedAt : {groupInfo.createdAt}</h1>
       <section>
-        <h1>Group Members </h1> 
-         {groupInfo.members ? groupInfo.members.map((userMember,index) => {
-           return (
-              <section key={index}>
-                    <img src={userMember.image} alt="user" />
-                    <h1>firstName : {userMember.firstName}</h1>
-                    <h1>lastName : {userMember.lastName}</h1>
-                    <h1>isLogin : {userMember.isLogin ? '✔' : 'false' }</h1>
-              </section>
-           )
-         }) : <h1>None</h1>}
+        <h1>Group Members </h1>
+        {groupInfo.members ? groupInfo.members.map((userMember, index) => {
+          return (
+            <section key={index}>
+              <img src={userMember.image} alt="user" />
+              <h1>firstName : {userMember.firstName}</h1>
+              <h1>lastName : {userMember.lastName}</h1>
+              <h1>isLogin : {userMember.isLogin ? '✔' : 'false'}</h1>
+            </section>
+          )
+        }) : <h1>None</h1>}
       </section>
 
-      
-      </section>
+
+    </section>
   )
 }
